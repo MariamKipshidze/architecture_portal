0f47960267a9433258ba2e0620eef0a412cf9577
mariam.kipshidze <mariam.kipshidze@swisscapital.ge>
2025-07-12 15:02:15 +0400

    Create project model

--- architecture_portal/project/models.py
+++ architecture_portal/project/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=200, blank=True)
+    client = models.CharField(max_length=200, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='project_images/')
+    caption = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"Image for {self.project.title}"
--- architecture_portal/project/views.py
+++ architecture_portal/project/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from project.models import Project
 
-# Create your views here.
+
+def project_list(request):
+    projects = Project.objects.filter(is_published=True).order_by('-start_date')
+    return render(request, 'project_list.html', {'projects': projects})
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'project_detail.html', {'project': project})
