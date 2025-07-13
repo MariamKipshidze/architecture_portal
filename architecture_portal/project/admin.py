618aa4ae75a3a7721f70877f8d4e86e2f8ad4597
mariam.kipshidze <mariam.kipshidze@swisscapital.ge>
2025-07-13 22:45:34 +0400

    Register Project and ProjectImage model in django admin

--- architecture_portal/project/admin.py
+++ architecture_portal/project/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from project.models import Project, ProjectImage
 
-# Register your models here.
+
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1  # Number of empty forms displayed
+    fields = ('image', 'caption')  # Fields to show inline
+    readonly_fields = ()
+    can_delete = True
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'client', 'location', 'start_date', 'end_date', 'is_published')
+    list_filter = ('is_published', 'start_date', 'end_date')
+    search_fields = ('title', 'client', 'location')
+    inlines = [ProjectImageInline]
