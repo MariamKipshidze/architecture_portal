097c8dda37fb6a1da53232f4c121b4c58864b925
mariam.kipshidze <mariam.kipshidze@swisscapital.ge>
2025-07-13 22:45:44 +0400

    Fixed import

--- architecture_portal/user/admin.py
+++ architecture_portal/user/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
-from .models import CustomUser
+from user.models import CustomUser
 
 
 @admin.register(CustomUser)
