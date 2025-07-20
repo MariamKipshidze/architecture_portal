d22492a022143854431c4c7e3556c9eba2c51d25
MariamKipshidze <mari.kifshidze@gmail.com>
2025-07-20 20:53:55 +0400

    Create TeamMember

--- architecture_portal/about_us/models.py
+++ architecture_portal/about_us/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class TeamMember(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=100)
+    position = models.CharField(verbose_name="Position", max_length=100)
+    bio = models.TextField(verbose_name="bio")
+    photo = models.ImageField(verbose_name="photo", upload_to='team_photos/')
+    is_active = models.BooleanField(verbose_name="Active", default=True)
+    order = models.PositiveIntegerField(verbose_name="Order", default=0)
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = "Team Member"
+        verbose_name_plural = "Team Members"
+
+    def __str__(self):
+        return f"{self.name} - {self.position}"
