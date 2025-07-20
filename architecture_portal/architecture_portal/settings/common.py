a8a5b572b0ce91083d12bf3e6f055a0ca9bf9ac9
MariamKipshidze <mari.kifshidze@gmail.com>
2025-07-20 20:27:50 +0400

    Load .env file in settings

--- architecture_portal/architecture_portal/settings/common.py
+++ architecture_portal/architecture_portal/settings/common.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()  # Load from .env file
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-key-for-ci-tests-only')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%4_fk-9gt_=x+n4)1%q+wiwre!covplkyh8)!bo_alk@ov*zgj'
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-key-for-ci-tests-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
