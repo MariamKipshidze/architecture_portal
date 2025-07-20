de3baf111b37bda588b0340ab2d07d1dfc339099
MariamKipshidze <mari.kifshidze@gmail.com>
2025-07-20 20:54:36 +0400

    Create TeamListView

--- architecture_portal/about_us/views.py
+++ architecture_portal/about_us/views.py
@@ -1,6 +1,17 @@
-# views.py
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from about_us.models import TeamMember
 
 
 def contact_view(request):
     return render(request, 'contact.html')
+
+
+class TeamListView(ListView):
+    model = TeamMember
+    template_name = 'team_list.html'
+    context_object_name = 'team_members'
+
+    def get_queryset(self):
+        return TeamMember.objects.filter(is_active=True).order_by('order')
