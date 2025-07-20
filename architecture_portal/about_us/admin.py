0b40ad8e9faa0339339c03b335c586779ac36f86
MariamKipshidze <mari.kifshidze@gmail.com>
2025-07-20 20:54:10 +0400

    Register TeamMember to django admin

--- architecture_portal/about_us/admin.py
+++ architecture_portal/about_us/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from about_us.models import TeamMember
 
-# Register your models here.
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'is_active', 'order')
+    list_filter = ('is_active', 'position')
+    search_fields = ('name', 'position', 'bio')
+    list_editable = ('order', 'is_active')
