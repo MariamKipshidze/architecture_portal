a6bdc7b48d36e5cd2c8c96e9f30a309ba38bfbc0
MariamKipshidze <mari.kifshidze@gmail.com>
2025-07-20 20:54:50 +0400

    Add TeamListView to urls

--- architecture_portal/about_us/urls.py
+++ architecture_portal/about_us/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from about_us.views import contact_view
+from about_us.views import contact_view, TeamListView
 
 urlpatterns = [
     path('contact/', contact_view, name='contact'),
+    path('team_members/', TeamListView.as_view(), name='team_list'),
 ]
