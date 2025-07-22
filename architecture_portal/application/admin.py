dbe2070e48bd2b66d2b73992a7482723c6878882
MariamKipshidze <mari.kifshidze@gmail.com>
2025-07-22 22:27:19 +0400

    Register Application in django admin

--- architecture_portal/application/admin.py
+++ architecture_portal/application/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from application.models import Application
 
-# Register your models here.
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('project_title', 'client_name', 'project_type', 'submission_date', 'viewed')
+    list_filter = ('viewed', 'project_type', 'submission_date')
+    search_fields = ('project_title', 'client_name', 'client_email')
+    actions = ['mark_as_processed']
+
+    def mark_as_processed(self, request, queryset):
+        queryset.update(viewed=True)
+
+    mark_as_processed.short_description = "Mark selected applications as processed"
