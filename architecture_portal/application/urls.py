26ac688e6fb369b2096d2215c0beb0a7623ee756
MariamKipshidze <mari.kifshidze@gmail.com>
2025-07-22 22:52:27 +0400

    Fixed urls

--- architecture_portal/application/urls.py
+++ architecture_portal/application/urls.py
@@ -3,8 +3,10 @@ from django.views.generic import TemplateView
 
 from application import views
 
+app_name = 'application'
+
 urlpatterns = [
     path('calculator/', views.calculate_price, name='calculate_price'),
-    path('application/', views.ApplicationCreateView.as_view(), name='application'),
+    path('create_application/', views.ApplicationCreateView.as_view(), name='create_application'),
     path('thank-you/', TemplateView.as_view(template_name='thank_you.html'), name='thank_you')
 ]
