d19b4449258e758d4afa1a14c5fd8816e35a34ef
MariamKipshidze <mari.kifshidze@gmail.com>
2025-07-22 22:36:08 +0400

    Register to url

--- architecture_portal/application/urls.py
+++ architecture_portal/application/urls.py
@@ -3,4 +3,6 @@ from application import views
 
 urlpatterns = [
     path('calculator/', views.calculate_price, name='calculate_price'),
+    path('application/', views.ApplicationCreateView.as_view(), name='application'),
+    path('thank-you/', views.TemplateView.as_view(template_name='thank_you.html'), name='thank_you')
 ]
